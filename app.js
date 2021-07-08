374b5c2352005724fe1dd2fbb8a04860d7f0fe00
huyntohoon <81351526+huyntohoon@users.noreply.github.com>
2021-07-08 16:45:41 +0900

    work_7/8

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+// canvas => html of Element, can access to pixel
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
-
 function stopPainting() {
   painting = false;
 }
-
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    //new path create
+    ctx.moveTo(x, y);
+    // move x, y, during not click
+  } else {
+    ctx.lineTo(x, y);
+    // (previous x, y to now x, y) to drawing
+    ctx.stroke();
+    // materialization to line
+  }
 }
 
-function onMouseDown(event) {
-  painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
-}
-function onMouseLeave(event) {
-  stopPainting();
-}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+function handleColorClick(event) {
+  const bgColor = event.target.style.backgroundColor;
+  ctx.strokeStyle = bgColor;
+}
+if (colors) {
+  Array.from(colors).forEach((color) =>
+    color.addEventListener("click", handleColorClick)
+  );
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+if (range) {
+  range.addEventListener("input", handleRangeChange);
 }
